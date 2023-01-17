d9f0f82c5a8f85d70004f9c5c7e192033a7ef775
Jakub Sepiolo <5sepij35@solent.ac.uk>
2023-01-17 18:04:38 +0000

    Tailwind working with template homepage

---
+++ app.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+const bodyParser = require("body-parser");
+
+
+
+
+
+/**
+ * Controllers (route handlers).
+ 
+const tasterController = require("./controllers/taster");
+const userController = require("./controllers/user");
+const tastingController = require("./controllers/tasting");
+const homeController = require("./controllers/home");
+*/
+const app = express();
+app.set("view engine", "ejs");
+
+/**
+ * notice above we are using dotenv. We can now pull the values from our environment
+ */
+
+const { WEB_PORT, MONGODB_URI } = process.env;
+
+/**
+ * connect to database
+ */
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+  console.log(
+    "MongoDB connection error. Please make sure MongoDB is running.",
+    chalk.red("✗")
+  );
+  process.exit();
+});
+
+/***
+ * We are applying our middlewear
+ */
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("index", { errors: {} });
+});
+
+app.listen(WEB_PORT, () => {
+  console.log(
+    `Example app listening at http://localhost:${WEB_PORT}`,
+    chalk.green("✓")
+  );
+});
